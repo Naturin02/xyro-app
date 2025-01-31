452bf526751bbef34b9ca2291ab319937153f72f
Naturin02 <arturo.realeza02@gmail.com>
2025-01-31 01:00:56 -0600

    config commit

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,10 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 export const API_URL = "http://192.168.0.87:5000";
+=======
+export const API_URL = "http://192.168.101.101:5000";
+>>>>>>> Stashed changes
 =======
 export const API_URL = "http://192.168.101.101:5000";
 >>>>>>> Stashed changes
