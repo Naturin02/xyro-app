f649805313a49a216d4ec5a4a1ef520d7c0bbb1d
Naturin02 <arturo.realeza02@gmail.com>
2025-02-24 23:39:31 -0600

    Update endpoints.ts

--- context/endpoints.ts
+++ context/endpoints.ts
@@ -1,2 +1,2 @@
-export const backend = "http://10.3.1.124:8000";   // Dirección IP red wifi
+export const backend = "http://10.2.9.223:8000";   // Dirección IP red wifi
 //export const backend = "http://192.168.101.102:8000";   // Dirección IP ethernet
